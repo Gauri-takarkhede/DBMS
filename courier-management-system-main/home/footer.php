517905916e5408c5e4e80548889e9bffb3718813
Gauri Takarkhede <111224258+Gauri-takarkhede@users.noreply.github.com>
2024-04-03 18:12:11 +0530

    Add files via upload

---
+++ courier-management-system-main/home/footer.php
@@ -0,0 +1,27 @@
+<!-- copyright and contactUs fixed footer -->
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.footer {
+    position: fixed;
+    width: 100%;
+    text-align: center;
+    
+    bottom: 0;
+    background-color: #034b54;
+    color: white;
+}
+</style>
+</head>
+<body>
+<footer class="footer">
+
+  <p style="margin-top: 4px;margin-bottom:-10px">Copyright © 2023 Designed By Gauri Takarkhede</p><br>
+  <a href="takarkhedegauri26@gmail.com" style="color:yellow">ContactUs</a>
+
+</footer>
+
+</body>
+</html>
